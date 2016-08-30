e095a39f02e80872611af60c3a842c3dcc989feb
suarkadipa <suarkadipa@gmail.com>
2016-08-30 13:27:11 +0800

    updated MibDefinitions

--- inc/MibDefinitions.hpp
+++ inc/MibDefinitions.hpp
@@ -9,7 +9,6 @@
 #ifndef INC_MIBDEFINITIONS_HPP_
 #define INC_MIBDEFINITIONS_HPP_
 
-
 // system description
 #define SYSTEM_DESCRIPTION "1.3.6.1.2.1.1.1.0"
 
@@ -20,21 +19,23 @@
 #define SYSTEM_UP_TIME "1.3.6.1.2.1.1.3.0"
 
 // System Contact
-#define SYSTEM_CONTACT	"1.3.6.1.2.1.1.4.0"
+#define SYSTEM_CONTACT "1.3.6.1.2.1.1.4.0"
 
 // system name
-#define SYSTEM_NAME		"1.3.6.1.2.1.1.5.0"
+#define SYSTEM_NAME "1.3.6.1.2.1.1.5.0"
 
 // System location
-#define SYSTEM_LOCATION	"1.3.6.1.2.1.1.6.0"
+#define SYSTEM_LOCATION "1.3.6.1.2.1.1.6.0"
 
 // Dhcp Server start time
 #define DHCP_MIB_PAR_DHCP_START_TIME ".1.3.6.1.4.1.311.1.3.1.1"
 
-// The total number of Messages delivered to the SNMP entity from the transport service.
+// The total number of Messages delivered to the SNMP entity from the transport
+// service.
 #define RFC1213_MIB_SNMP_IN_PKTS ".1.3.6.1.2.1.11.1"
 
-// The total number of SNMP Trap PDUs which have been accepted and processed by the SNMP protocol entity.
+// The total number of SNMP Trap PDUs which have been accepted and processed by
+// the SNMP protocol entity.
 #define RFC1213_MIB_SNMP_IN_TRAPS ".1.3.6.1.2.1.11.19"
 
 //  The amount of physical main memory contained by the host.
@@ -43,7 +44,8 @@
 // The product ID of the firmware associated with the processor.
 #define HOST_RESOURCES_MIB_HR_PROCESSOR_FRW_ID ".1.3.6.1.2.1.25.3.3.1.1"
 
-// The average, over the last minute, of the percentage of time that this processor was not idle.
+// The average, over the last minute, of the percentage of time that this
+// processor was not idle.
 #define HOST_RESOURCES_MIB_HR_PROCESSOR_LOAD ".1.3.6.1.2.1.25.3.3.1.2"
 
 /*  The current status of this printer device.
@@ -58,10 +60,12 @@
 // The type of storage represented by this entry.
 #define HOST_RESOURCES_MIB_HR_STORAGE_TYPE ".1.3.6.1.2.1.25.2.3.1.2"
 
-//  A description of the type and instance of the storage described by this entry.
+//  A description of the type and instance of the storage described by this
+//  entry.
 #define HOST_RESOURCES_MIB_HR_STORAGE_DESCR ".1.3.6.1.2.1.25.2.3.1.3"
 
-//  The amount of the storage represented by this entry that is allocated, in units of hrStorageAllocationUnits.
+//  The amount of the storage represented by this entry that is allocated, in
+//  units of hrStorageAllocationUnits.
 #define HOST_RESOURCES_MIB_HR_STORAGE_USED ".1.3.6.1.2.1.25.2.3.1.6"
 
 /*  The size, in bytes, of the data objects allocated
@@ -78,6 +82,6 @@
  * such an address (e.g., a serial line), this object
  * should contain an octet string of zero length.
  */
-#define  RFC1213_MIB_ IF_PHYS_ADDRESS ".1.3.6.1.2.1.2.2.1.6"
+#define RFC1213_MIB_IF_PHYS_ADDRESS ".1.3.6.1.2.1.2.2.1.6"
 
 #endif /* INC_MIBDEFINITIONS_HPP_ */
